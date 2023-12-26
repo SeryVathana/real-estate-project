ce164c43edf2bf8bfc6a6e3469fb76d01ff77972
SeryVathana <yooseryvathana@gmail.com>
2023-12-26 07:57:39 +0700

    Fix Daisy UI

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -6,10 +6,10 @@ function Homepage() {
     <MainLayout>
       <div>
         <div className='max-w-screen-2xl mx-auto px-2 flex flex-col justify-center items-center py-20'>
-          <h1 className=' text-5xl font-semibold text-center max-w-[700px] leading-tight'>
+          <h1 className=' text-4xl md:text-5xl font-semibold text-center max-w-[700px] leading-tight'>
             FIND YOUR DREAM HOME
           </h1>
-          <p className='text-center text-xl text-gray-500 max-w-[700px] mt-5'>
+          <p className='text-center text-sm md:text-xl text-gray-500 max-w-[700px] mt-5'>
             If you're looking for a place where you can be yourself, don't give up. Keep searching
             until you find a place that feels like home
           </p>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,8 @@
 function Nav() {
   return (
     <div className='max-w-screen border-b border-gray-300'>
-      <div className='navbar bg-base-100 max-w-screen-2xl mx-auto'>
-        <div className='navbar-start'>
+      <div className='navbar bg-base-100 flex justify-between max-w-screen-2xl mx-auto'>
+        <div className=''>
           <div className='lg:hidden'>
             <div className='dropdown'>
               <div tabIndex={0} role='button' className='btn btn-ghost'>
@@ -52,7 +52,7 @@ function Nav() {
             Real Estate
           </a>
         </div>
-        <div className='navbar-center hidden lg:flex'>
+        <div className=' hidden lg:flex'>
           <ul className='menu menu-horizontal px-1'>
             <li>
               <a href='/browse'>Browse</a>
@@ -75,9 +75,9 @@ function Nav() {
             </li>
           </ul>
         </div>
-        <div className='navbar-end flex gap-4'>
+        <div className=' flex gap-4'>
           <div className='hidden md:block'>
-            <button className='btn btn-neutral'>Create an account</button>
+            <button className='btn btn-primary'>Create an account</button>
           </div>
           <button className='btn btn-outline'>Sign In</button>
         </div>
