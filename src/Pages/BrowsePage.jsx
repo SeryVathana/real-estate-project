da1bb7d507663f69c14625ce6be401f8d744e04b
seungsovannary <seungaovananry527@gmail.com>
2023-12-29 11:28:23 +0700

    Update BrowsePage.jsx

--- src/Pages/BrowsePage.jsx
+++ src/Pages/BrowsePage.jsx
@@ -1,7 +1,18 @@
+import { Menu } from 'lucide-react';
 import MainLayout from '../Layouts/MainLayout';
 
+
 const BrowsePage = () => {
-  return <MainLayout>BrowsePage</MainLayout>;
-};
+  return <MainLayout >
+    <div className='max-w-screen-2xl mx-auto px-2 flex flex-col justify-center items-center py-20'></div>
+      
+        
+        
+          </MainLayout>;
+        };
+
+
+
+
 
 export default BrowsePage;
